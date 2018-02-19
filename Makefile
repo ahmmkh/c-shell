74308304b1290e54fb53513b51591c7d57fd6eee
AHmed Khatab <b-b@dr.com>
2018-02-19 20:48:46 +0200

    added make file

---
+++ Makefile
@@ -0,0 +1,2 @@
+shell:myshell.c
+	cc myshell.c lex.yy.c -lfl -o myshell
--- myshell.c
+++ myshell.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
 
 extern char **getline();
 
-int main() {
-  int i;
-  char **input;
+int main()
+{
+    int i;
+    char **input;
 
-  while (1) {
-    input = getline();
-    for (int i = 0; input[i] != NULL; ++i) {
-      printf("Item %i of input: %s\n", i, input[i]); // The input list that must be parsed. 
+    while (1)
+    {
+        input = getline();
+        for (int i = 0; input[i] != NULL; ++i)
+        {
+            printf("Item %i of input: %s\n", i, input[i]); // The input list that must be parsed.
+        }
     }
-  }
-  
-  return 0;
+    return 0;
 }
