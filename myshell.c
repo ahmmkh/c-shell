5603e1559dd93be4661dec893d362a1cba8fea81
AHmed Khatab <b-b@dr.com>
2018-02-23 19:11:31 +0200

    directory

--- myshell.c
+++ myshell.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
-#define BUFFSIZE 512
+#define BUFFSIZE 1024
 extern char **getline();
 
 int main()
@@ -9,12 +9,21 @@ int main()
     int i;
     char buffer[BUFFSIZE];
     char **input;
+    char pwd[BUFFSIZE];
     while (1)
     {
+        if (getcwd(pwd, sizeof(pwd)) != NULL)
+        {
+            printf("myshell@freebsd:%s", pwd);
+        }
+        else
+        {
+            printf("error with getcwd");
+        }
         input = getline();
         for (int i = 0; input[i] != NULL; ++i)
         {
-            if (input[0] == *"cd" )
+            if (*input[0] == *("cd"))
             {
                 printf("done");
             }
